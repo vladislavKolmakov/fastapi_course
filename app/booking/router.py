d4acb26494082144fddd31433d89f2bdfa6e3812
Vladislav Kolmakov <imfoslash@gmail.com>
2023-05-07 14:04:14 +0300

    replace slalars() with mapping()

--- app/booking/router.py
+++ app/booking/router.py
@@ -11,3 +11,7 @@ router = APIRouter(
 @router.get('')
 async def get_booking() -> list[SBooking]:
         return await BookingDAO.find_all()
+
+@router.get('')
+async def get_booking() -> SBooking:
+        return await BookingDAO.find_one_or_none(room_id=7)
--- app/dao/dase.py
+++ app/dao/dase.py
@@ -6,24 +6,24 @@ from app.database import async_session_maker
 class BaseDAO:
     model = None
 
-    @classmethod
-    async def find_by_id(cls, model_id: int):
-        async with async_session_maker() as session:
-            query = select(cls.model).filter_by(id=model_id)
-            result = await session.execute(query)
-            return result.scalar_one_or_none() 
+    # @classmethod
+    # async def find_one_or_none(cls, **filter):
+    #     async with async_session_maker() as session:
+    #         query = select(cls.model.__table__.columns).filter_by(**filter)
+    #         result = await session.execute(query)
+    #         return result.mappings().one_or_none()
 
     @classmethod
     async def find_one_or_none(cls, **filter):
-        async with async_session_maker() as sesssion:
-            query = select(cls.model).filter_by(**filter)
-            result = await sesssion.execute(query)
-            return result.scalar_one_or_none()
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
 
 
     @classmethod
-    async def find_all(cls, **filter_by):
+    async def find_all(cls, **filter):
         async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filter_by)
+            query = select(cls.model.__table__.columns).filter_by(**filter)
             result = await session.execute(query)
-            return result.scalars().all()
+            return result.mappings().all()
